4a12cdc8569bd9a6ce0cb298ecd27e3d9c1aa09c
pisey1993 <pisey@peoplenpartners.com>
2025-06-09 14:09:24 +0700

    update logout

--- views/view-claims.php
+++ views/view-claims.php
@@ -1,8 +1,8 @@
 <?php
 session_start(); // ⬅️ First line, no whitespace above it
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 require_once '../config/db.php';
 require_once '../models/ClaimsOnline.php';
@@ -29,7 +29,7 @@ function safeEcho($val) {
 
 $uploadDir = __DIR__ . "/../uploads/claims/" . $id . "/";
 //$uploadUrl = "/crm/uploads/claims/" . $id . "/";
-$uploadUrl = "/client/uploads/claims/" . $id . "/";
+$uploadUrl = "client/uploads/claims/" . $id . "/";
 $files = [];
 
 if (is_dir($uploadDir)) {
